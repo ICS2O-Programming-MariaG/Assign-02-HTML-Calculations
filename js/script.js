e3b27f47efa5faa03e5910746c72ae48aa6102ae
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-01 15:04:05 +0000

    Added parseFloat to variables in js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 function enterClicked() {
   const CONVERSION_RATE = 0.8;
-  let dollarsCad = document.getElementById("money-cad").value;
-  let days = document.getElementById("days-in-us").value;
+  let dollarsCad = parseFloat(document.getElementById("money-cad").value);
+  let days = parseFloat(document.getElementById("days-in-us").value);
   let dollarsUsd = dollarsCad * CONVERSION_RATE;
   document.getElementById("results").innerHTML = "The amount of money you will have for your trip in USD is $" + dollarsUsd.toFixed(2) + ".";
 }
